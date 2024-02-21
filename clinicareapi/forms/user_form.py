f4cf176686e19b281d33ead6c19ede47344c8abb
lucasops96 <lucasbanana96@gmail.com>
2024-02-20 22:35:18 -0300

    criado metodo post ProfissionalUpdateView

--- clinicareapi/forms/user_form.py
+++ clinicareapi/forms/user_form.py
@@ -80,3 +80,66 @@ class UserForm(forms.ModelForm):
         
         if password != password2:
             self.add_error('password','Senha deve ser igual Confirme sua Senha.')
+
+
+
+
+class UserUpdateForm(forms.ModelForm):
+    
+    username =  forms.CharField(
+        label='Username',
+        error_messages={
+            'required':'This field must not be empty'
+        },
+        widget= forms.TextInput(attrs={'placeholder':'Seu username'}),
+        help_text=(
+            'O nome de usuário deve conter letras, números ou um desses @.+-_.'
+        ),
+    )
+
+    email = forms.EmailField(
+        label='E-mail',
+        help_text='O e-mail deve ser válido',
+        widget= forms.EmailInput(attrs={'placeholder':'Seu e-mail'}),
+        error_messages={'required': 'E-mail is required'},
+    )
+
+    first_name = forms.CharField(
+        label='Primeiro Nome',
+        error_messages={'required': 'Write your first name'},
+        widget= forms.TextInput(attrs={'placeholder':'Ex.: Miguel'})
+    )
+
+    last_name = forms.CharField(
+        label='Último Nome',
+        error_messages={'required':'Write your last name'},
+        widget= forms.TextInput(attrs={'placeholder':'Ex.: Silva'})
+    )
+
+    # password = forms.CharField(
+    #     widget=forms.PasswordInput(attrs={'placeholder':'Digite uma senha'}),
+    #     label='Senha',
+    #     error_messages={'required':'Password must not be empty'},
+    #     help_text=(
+    #         'A senha deve ter pelo menos uma letra maiúscula,  '
+    #         'uma letra minúscula e um número. Com a quantidade'
+    #         'de pelo menos 8 caracteres.'
+    #     ),
+    # )
+
+    # password2 = forms.CharField(
+    #     widget=forms.PasswordInput(attrs={'placeholder':'Repita sua senha'}),
+    #     label='Confirme sua Senha',
+    #     error_messages={
+    #         'required':'Please, repeat your password'
+    #     },
+    # )
+    
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+        ]
--- clinicareapi/views/site/profissional_saude_view.py
+++ clinicareapi/views/site/profissional_saude_view.py
@@ -6,9 +6,10 @@ from django.views.generic import CreateView , ListView, DetailView, UpdateView
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.contrib import messages
+from django.http.response import Http404
 from ...models import ProfissionalSaude, CustomUser, User
 from ...forms.profissional_form import ProfissionalSaudeForm
-from ...forms.user_form import UserForm
+from ...forms.user_form import UserForm, UserUpdateForm
 from ...forms.custom_user_form import CustomUserForm
 
 class ProfissionalSaudeListView(ListView):
@@ -89,5 +90,44 @@ class ProfissionalSaudeUpdateView(UpdateView):
         profissional = ProfissionalSaude.objects.filter(id=pk).first()
         form = ProfissionalSaudeForm(instance=profissional)
         form_custom = CustomUserForm(instance=profissional.user_profissional)
-        form_user = UserForm(instance=profissional.user_profissional.user_custom)
+        form_user = UserUpdateForm(instance=profissional.user_profissional.user_custom)
+        return self.render_forms(form,form_user,form_custom)
+    
+    def post(self,request,pk):
+        if request.user.user_custom.id_profissional != pk:
+            raise Http404()
+        
+        profissional = ProfissionalSaude.objects.filter(id=pk).first()
+        
+        form_user = UserUpdateForm(
+            data=request.POST or None,
+            files=request.FILES or None, 
+            instance=profissional.user_profissional.user_custom
+        )
+        form_custom = CustomUserForm(
+            data=request.POST or None,
+            files=request.FILES or None,
+            instance=profissional.user_profissional
+        )
+        form = ProfissionalSaudeForm(
+            data=request.POST or None,
+            files=request.FILES or None,
+            instance=profissional
+        )
+
+        if form_user.is_valid() and form_custom.is_valid() and form.is_valid():
+            user = form_user.save()
+            custom = form_custom.save(commit=False)
+            custom.user_custom = user
+            custom.save()
+            profissional = form.save(commit=False)
+            profissional.user_profissional = custom
+            profissional.save()
+
+            messages.success(request,'Alterações realizada com sucesso!')
+
+            return redirect(
+                reverse('clinicareapi:profissional_update_view',kwargs={'pk':profissional.id})
+            )
+        
         return self.render_forms(form,form_user,form_custom)
